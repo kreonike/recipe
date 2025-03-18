bb5b414f725804ca70ccea445ee1ce8128ac85ac
kreon <kreonike@gmail.com>
2025-03-18 21:56:46 +0300

    module03

---
+++ module_03_ci_culture_beginning/homework/hw2/tests/decrypt_test.py
@@ -0,0 +1,37 @@
+import unittest
+from decrypt import decrypt
+
+
+class TestDecrypt(unittest.TestCase):
+    def test_no_dots(self):
+        self.assertEqual(decrypt('абра-кадабра'), 'абра-кадабра')
+
+    def test_single_dot(self):
+        self.assertEqual(decrypt('абраа.-кадабра'), 'абраа-кадабра')
+
+    def test_two_dots(self):
+        with self.subTest():
+            self.assertEqual(decrypt('абраа..-кадабра'), 'абра-кадабра')
+        with self.subTest():
+            self.assertEqual(decrypt('абраа..-.кадабра'), 'абра-кадабра')
+        with self.subTest():
+            self.assertEqual(decrypt('абра--..кадабра'), 'абра-кадабра')
+
+    def test_three_dots(self):
+        self.assertEqual(decrypt('абрау...-кадабра'), 'абра-кадабра')
+
+    def test_multiple_dots(self):
+        with self.subTest():
+            self.assertEqual(decrypt('абра........'), '')
+        with self.subTest():
+            self.assertEqual(decrypt('абр......a.'), 'a')
+        with self.subTest():
+            self.assertEqual(decrypt('1..2.3'), '23')
+        with self.subTest():
+            self.assertEqual(decrypt('.'), '')
+        with self.subTest():
+            self.assertEqual(decrypt('1.......................'), '')
+
+
+if __name__ == '__main__':
+    unittest.main()
