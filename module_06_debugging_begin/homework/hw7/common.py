e241d49b22af941fc134f85b00327c2834429da5
kreonike <kreonike@gmail.com>
2025-03-21 08:23:45 +0300

    module06 03

--- module_06_debugging_begin/homework/hw7/common.py
+++ module_06_debugging_begin/homework/hw7/common.py
@@ -13,10 +13,10 @@ def configure_logging(level=logging.INFO):
                                   '"message": "%(message)s"}',
                                   datefmt='%Y-%m-%d %H:%M:%S')
 
-    file_handler = logging.FileHandler('skillbox_json_messages.log', mode='w')
-    file_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
 
     logging.basicConfig(
         level=level,
-        handlers=[file_handler]
+        handlers=[console_handler]
     )
--- module_06_debugging_begin/homework/hw8/my_t9.py
+++ module_06_debugging_begin/homework/hw8/my_t9.py
@@ -19,12 +19,37 @@
 """
 from typing import List
 
+# Сопоставление цифр и букв
+digit_to_letters = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+}
 
 def my_t9(input_numbers: str) -> List[str]:
-    ...
+    with open('words', 'r') as file:
+        english_words = set(word.strip().lower() for word in file.readlines())
 
+    def generate_combinations(digits, current_combination, index):
+        if index == len(digits):
+            combinations.append(current_combination)
+            return
+        for letter in digit_to_letters[digits[index]]:
+            generate_combinations(digits, current_combination + letter, index + 1)
+
+    combinations = []
+    generate_combinations(input_numbers, "", 0)
+
+    valid_words = [word for word in combinations if word in english_words]
+
+    return valid_words
 
 if __name__ == '__main__':
-    numbers: str = input()
+    numbers: str = input('Введите последовательность цифр (2-9): ')
     words: List[str] = my_t9(numbers)
     print(*words, sep='\n')
