09f80915df4cac4807d88eff94e69a8549a6ad20
kreonike <kreonike@gmail.com>
2025-03-25 10:57:55 +0300

    module10

--- module_10_db1/homework/hw5/quick_insert.py
+++ module_10_db1/homework/hw5/quick_insert.py
@@ -4,7 +4,16 @@ Number = Union[int, float, complex]
 
 
 def find_insert_position(array: List[Number], number: Number) -> int:
-    ...
+    left, right = 0, len(array)
+
+    while left < right:
+        mid = (left + right) // 2
+        if array[mid] < number:
+            left = mid + 1
+        else:
+            right = mid
+
+    return left
 
 
 if __name__ == '__main__':
@@ -17,3 +26,7 @@ if __name__ == '__main__':
     x: Number = 4
     A.insert(insert_position, x)
     assert A == sorted(A)
+    assert find_insert_position([], 10) == 0
+    assert find_insert_position([1, 2, 3], 0) == 0
+    assert find_insert_position([1, 2, 3], 4) == 3
+    assert find_insert_position([1, 2, 2, 3], 2) in [1, 2, 3]
