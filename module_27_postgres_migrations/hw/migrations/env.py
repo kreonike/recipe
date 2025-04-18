64f9a79ebe807f6d21625404446ee9141a25867c
kreonike <kreonike@gmail.com>
2025-04-18 09:32:00 +0300

    module27 fix

--- module_27_postgres_migrations/hw/migrations/env.py
+++ module_27_postgres_migrations/hw/migrations/env.py
@@ -4,6 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +21,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models import db
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
