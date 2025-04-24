11d3cc8c2bfa05916b33b14d79dde03b156f739c
kreonike <kreonike@gmail.com>
2025-04-24 15:39:40 +0300

    Delete module_14_mvc directory

--- module_14_mvc/homework/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-
-class BookForm(FlaskForm):
-    title = StringField('Book Title', validators=[DataRequired()])
-    author = StringField('Author', validators=[DataRequired()])
