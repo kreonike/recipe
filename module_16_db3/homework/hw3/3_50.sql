bbcefb78a968fece4acfaf762c8dc448f4257214
kreonike <kreonike@gmail.com>
2025-04-24 15:40:00 +0300

    Delete module_16_db3 directory

--- module_16_db3/homework/hw3/3_50.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT o.battle
-FROM Outcomes o
-JOIN Ships s ON o.ship = s.name
-WHERE s.class = 'Kongo'
-UNION
-SELECT DISTINCT o.battle
-FROM Outcomes o
-WHERE o.ship = 'Kongo';
--- module_16_db3/homework/hw3/3_7.sql
+++
@@ -1,8 +0,0 @@
-SELECT model, price FROM PC
-WHERE model IN (SELECT model FROM Product WHERE maker = 'B')
-UNION
-SELECT model, price FROM Laptop
-WHERE model IN (SELECT model FROM Product WHERE maker = 'B')
-UNION
-SELECT model, price FROM Printer
-WHERE model IN (SELECT model FROM Product WHERE maker = 'B');
