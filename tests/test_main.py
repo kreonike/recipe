57fa8e4c974523cc0b4bf7a5d455b5ca8cea6850
megio <kreonike@gmail.com>
2025-04-24 17:01:24 +0300

    Fix flake8 errors: add newline at end of files and remove unused import

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from main import app, get_db
-from models import Base, Recipe
+from models import Base
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
