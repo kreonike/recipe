578208c680af5beaf2dd5d44b6950525cd48e9f1
megio <kreonike@gmail.com>
2025-04-24 17:37:29 +0300

    27-3

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel, ConfigDict
 
 Base = declarative_base()
 
+
 class Recipe(Base):
     __tablename__ = "recipes"
     id = Column(Integer, primary_key=True, index=True)
@@ -13,20 +14,24 @@ class Recipe(Base):
     description = Column(Text, nullable=False)
     views = Column(Integer, default=0)
 
+
 class RecipeBase(BaseModel):
     name: str
     cooking_time: int
     ingredients: str
     description: str
 
+
 class RecipeCreate(RecipeBase):
     pass
+
 
 class RecipeList(RecipeBase):
     id: int
     views: int
 
     model_config = ConfigDict(from_attributes=True)
+
 
 class RecipeDetail(RecipeList):
     pass
